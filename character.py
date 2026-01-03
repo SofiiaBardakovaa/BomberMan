1eec91d87c5a2d8c286202af747305929a508792
Софія Бардакова <sofiia.bardakova@nure.ua>
2026-01-04 00:28:36 +0200

    Moving the Player Character

--- character.py
+++ character.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.key import get_pressed
+
 import gamesettings as gs
 
 class Character(pygame.sprite.Sprite):
@@ -12,6 +14,7 @@ class Character(pygame.sprite.Sprite):
 
         #Character attributes
         self.alive = True
+        self.speed = 3
 
         # Character Display
         self.image = None
@@ -25,6 +28,19 @@ class Character(pygame.sprite.Sprite):
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.GAME.MAIN.run = False
+
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_RIGHT]:
+            self.x += self.speed
+        elif keys_pressed[pygame.K_a] or keys_pressed[pygame.K_LEFT]:
+            self.x -= self.speed
+        elif keys_pressed[pygame.K_w] or keys_pressed[pygame.K_UP]:
+            self.y -= self.speed
+        elif keys_pressed[pygame.K_s] or keys_pressed[pygame.K_DOWN]:
+            self.y += self.speed
+
+        # update the character rect position
+        self.rect.topleft = (self.x, self.y)
 
     def update(self):
         pass
