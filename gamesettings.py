40ee61c3c7c31b36c7cedbb7dac4dda2b9afd0ad
Софія Бардакова <sofiia.bardakova@nure.ua>
2026-01-03 22:28:34 +0200

    Basic settings

---
+++ gamesettings.py
@@ -0,0 +1,13 @@
+from turtledemo.nim import SCREENWIDTH, SCREENHEIGHT
+
+# Game Window Settings
+SCREENWIDTH = 1290
+SCREENHEIGHT = 892
+
+# Game Frames per Second
+
+FPS = 60
+
+# Colours
+
+BLACK = (0, 0, 0)
